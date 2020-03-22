226b4408db901458261a93a61168c13a5cb441ea
npapiri <48774899+npapiri@users.noreply.github.com>
2020-03-22 16:30:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import altair as alt
 
 
 
+
 url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
 
 querystring = {"country":"US"}
@@ -19,24 +20,27 @@ headers = {
     }
 
 
+
 response= requests.get(url,headers=headers, params=querystring)
 
 response.json()
 
 json_res= response.json()
 
-
 df1 = json_normalize(json_res,['data','covid19Stats'])
 
 
-st.title('COVID19 Data via API')
+st.title('COVID19 Data (via API)')
 
-st.title(sum(df1.deaths))
+st.subheader('Total Deaths:')
+st.write(sum(df1.deaths))
 
-st.title(sum(df1.confirmed))
+st.subheader('Total Confirmed')
+st.write(sum(df1.confirmed))
 
 if st.checkbox('Show Data'):
     st.write(df1,height=1000, length =1000)
+
 
 if st.checkbox('deaths'):
      c = alt.Chart(df1, width=1000, height=1000).mark_bar(clip=True).encode(x='province', y='deaths')
@@ -47,5 +51,5 @@ if st.checkbox('recovered'):
      st.altair_chart(c)
 
 if st.checkbox('confirmed'):
-     c = alt.Chart(df1, width=800, height=800).mark_bar(clip=True).encode(x='province', y='confirmed')
+     c = alt.Chart(df1, width=1000, height=1000).mark_bar(clip=True).encode(x='province', y='confirmed')
      st.altair_chart(c)
